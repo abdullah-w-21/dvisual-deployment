3b3d59b2c825052cd7b2ca674a0bea7d3e935961
Abdullah Wasim <81172855+abdullah-w-21@users.noreply.github.com>
2023-11-25 11:15:02 -0800

    Update Navbar.jsx

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 
-const baseUrl = process.env.URLL;
+const baseUrl = process.env.URLL || '';
 const Navbar = () => {
   const [login, setLogin] = useState(false);
   const [forceRender, setForceRender] = useState(false);
