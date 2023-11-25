95b1bb8b7a5bc18aa259e76613f094c6b145dbfb
Abdullah Wasim <81172855+abdullah-w-21@users.noreply.github.com>
2023-11-25 11:08:14 -0800

    Update Logout.jsx

--- client/src/component/Logout.jsx
+++ client/src/component/Logout.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
+const baseUrl = process.env.URLL;
+
 const Logout = () => {
   const history = useHistory();
 
